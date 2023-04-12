cad8365d94242bff6d0f195e38eb881fd8697ffa
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-04-11 20:49:18 -0500

    first crack at server file

---
+++ server.js
@@ -0,0 +1,63 @@
+// Import Express.js
+const express = require('express');
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require('path');
+
+// Initialize an instance of Express.js
+const app = express();
+// Import Express.js
+const express = require('express');
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require('path');
+
+// Initialize an instance of Express.js
+const app = express();
+
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/Yankees', (req, res) => res.send('The Champs of 2023 /routes'));
+
+app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
+
+app.get('/routes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/routes.html'))
+);
+
+// listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/Yankees', (req, res) => res.send('The Champs of 2023 /routes'));
+
+app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
+
+app.get('/routes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/routes.html'))
+);
+
+// listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
